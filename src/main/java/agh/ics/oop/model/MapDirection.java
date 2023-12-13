5ef0b51181eb848e78525b0fdbc85f2dde82869e
RCRalph <rcralph@rcralph.me>
2023-12-13 12:05:56 +0100

    Extended MapDirection to include intermediate directions, added rotateByGene method

--- src/main/java/agh/ics/oop/model/MapDirection.java
+++ src/main/java/agh/ics/oop/model/MapDirection.java
@@ -2,42 +2,32 @@ package agh.ics.oop.model;
 
 public enum MapDirection {
     NORTH(new Vector2D(0, 1), "N"),
+    NORTH_EAST(new Vector2D(1, 1), "NE"),
+    EAST(new Vector2D(1, 0), "E"),
+    SOUTH_EAST(new Vector2D(1, -1), "SE"),
     SOUTH(new Vector2D(0, -1), "S"),
+    SOUTH_WEST(new Vector2D(-1, -1), "SW"),
     WEST(new Vector2D(-1, 0), "W"),
-    EAST(new Vector2D(1, 0), "E");
+    NORTH_WEST(new Vector2D(-1, 1), "NW");
 
-    private final Vector2D unitVector;
+    private final Vector2D squareVector;
 
     private final String label;
 
-    MapDirection(Vector2D unitVector, String label) {
-        this.unitVector = unitVector;
+    MapDirection(Vector2D squareVector, String label) {
+        this.squareVector = squareVector;
         this.label = label;
     }
 
-    public MapDirection next() {
-        return switch (this) {
-            case NORTH -> EAST;
-            case EAST -> SOUTH;
-            case SOUTH -> WEST;
-            case WEST -> NORTH;
-        };
-    }
-
-    public MapDirection previous() {
-        return switch (this) {
-            case NORTH -> WEST;
-            case WEST -> SOUTH;
-            case SOUTH -> EAST;
-            case EAST -> NORTH;
-        };
+    public MapDirection rotateByGene(Gene gene) {
+        return MapDirection.values()[(this.ordinal() + gene.ordinal()) % MapDirection.values().length];
     }
 
     public String toString() {
         return this.label;
     }
 
-    public Vector2D toUnitVector() {
-        return this.unitVector;
+    public Vector2D toSquareVector() {
+        return this.squareVector;
     }
 }
