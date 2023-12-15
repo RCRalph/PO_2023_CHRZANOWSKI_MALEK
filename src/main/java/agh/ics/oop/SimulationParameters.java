24a0af5fa3e585648df9c97392875f28c40ee829
RCRalph <rcralph@rcralph.me>
2023-12-15 22:38:20 +0100

    Added empty interfaces for simulation parameters

--- src/main/java/agh/ics/oop/SimulationParameters.java
+++ src/main/java/agh/ics/oop/SimulationParameters.java
@@ -1,45 +1,45 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.map.AnimalBehaviourIndicator;
+import agh.ics.oop.model.map.MutationIndicator;
+import agh.ics.oop.model.map.PlantGrowthVariant;
 import agh.ics.oop.model.map.WorldMap;
 
 public record SimulationParameters(
     int mapWidth,
     int mapHeight,
-    WorldMap mapVariant,
+    Class<WorldMap> worldMapClass,
     int startPlantCount,
     int plantEnergy,
     int dailyPlantGrowth,
     int startAnimalCount,
+    int startAnimalEnergy,
     int healthyAnimalEnergy,
     int reproductionEnergy,
-    int geneCount,
     int minimumMutationCount,
-    int maximumMutationCount
-    // plantGrowthVariant
-    // mutationVariant
-    // behaviourVariant
+    int maximumMutationCount,
+    Class<PlantGrowthVariant> plantGrowthVariantClass,
+    Class<MutationIndicator> mutationIndicatorClass,
+    int geneCount,
+    Class<AnimalBehaviourIndicator> animalBehaviourIndicatorClass
 ) {
-    private record Assertion(
-       boolean predicate,
-       String message
-    ) {}
-
     public SimulationParameters {
-        Assertion[] asserts = new Assertion[] {
-            new Assertion(this.mapWidth() <= 0, "Map width should be greater than 0"),
-            new Assertion(this.mapHeight() <= 0, "Map height should be greater than 0"),
-            new Assertion(this.startPlantCount() < 0, "Start plant count should be greater than or equal to 0"),
-            new Assertion(this.dailyPlantGrowth() < 0, "Daily plant growth should be greater than or equal to 0"),
-            new Assertion(this.startAnimalCount() <= 0, "Start animal count should be greater than 0"),
-            new Assertion(this.geneCount() <= 0, "Gene count should be greater than 0"),
-            new Assertion(this.minimumMutationCount() < 0, "Minimal mutation count should be greater or equal to 0"),
-            new Assertion(this.maximumMutationCount() > this.geneCount(), "Maximal mutation count should be less than or equal to gene count")
-        };
-
-        for (Assertion item : asserts) {
-            if (item.predicate) {
-                throw new IllegalArgumentException(item.message);
-            }
+        if (this.mapWidth() <= 0) {
+            throw new IllegalArgumentException("Map width should be greater than 0");
+        } else if (this.mapHeight() <= 0) {
+            throw new IllegalArgumentException("Map height should be greater than 0");
+        } else if (this.startPlantCount() < 0) {
+            throw new IllegalArgumentException("Start plant count should be greater than or equal to 0");
+        } else if (this.dailyPlantGrowth() < 0) {
+            throw new IllegalArgumentException("Daily plant growth should be greater than or equal to 0");
+        } else if (this.startAnimalCount() <= 0) {
+            throw new IllegalArgumentException("Start animal count should be greater than 0");
+        } else if (this.minimumMutationCount() < 0) {
+            throw new IllegalArgumentException("Minimal mutation count should be greater or equal to 0");
+        } else if (this.maximumMutationCount() > this.geneCount()) {
+            throw new IllegalArgumentException("Maximal mutation count should be less than or equal to gene count");
+        } else if (this.geneCount() <= 0) {
+            throw new IllegalArgumentException("Gene count should be greater than 0");
         }
     }
 }
