d5c00a29b7c19db77e38a418a06c65f16debf5fe
RCRalph <rcralph@rcralph.me>
2023-12-19 14:16:23 +0100

    Updated Simulation constructor

--- src/main/java/agh/ics/oop/Simulation.java
+++ src/main/java/agh/ics/oop/Simulation.java
@@ -1,52 +1,59 @@
 package agh.ics.oop;
 
-import agh.ics.oop.model.MoveDirection;
-import agh.ics.oop.model.Vector2D;
-import agh.ics.oop.model.element.Animal;
-import agh.ics.oop.model.map.PositionAlreadyOccupiedException;
+import agh.ics.oop.model.element.BehaviourIndicator;
+import agh.ics.oop.model.element.GenesIndicator;
 import agh.ics.oop.model.map.WorldMap;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Simulation implements Runnable {
-    private final List<Animal> animals;
-
     private final WorldMap map;
 
-    private final List<MoveDirection> moves;
+    private final GenesIndicator genesIndicator;
 
-    public Simulation(List<MoveDirection> moves, List<Vector2D> positions, WorldMap map) {
-        if (positions.isEmpty()) {
-            throw new IllegalArgumentException("Positions cannot be empty");
+    private final BehaviourIndicator behaviourIndicator;
+
+    public Simulation(SimulationParameters parameters) {
+        if (parameters.mapWidth() <= 0) {
+            throw new IllegalArgumentException("Map width should be greater than 0");
+        } else if (parameters.mapHeight() <= 0) {
+            throw new IllegalArgumentException("Map height should be greater than 0");
+        } else if (parameters.startPlantCount() < 0) {
+            throw new IllegalArgumentException("Start plant count should be greater than or equal to 0");
+        } else if (parameters.dailyPlantGrowth() < 0) {
+            throw new IllegalArgumentException("Daily plant growth should be greater than or equal to 0");
+        } else if (parameters.startAnimalCount() <= 0) {
+            throw new IllegalArgumentException("Start animal count should be greater than 0");
+        } else if (parameters.minimumMutationCount() < 0) {
+            throw new IllegalArgumentException("Minimal mutation count should be greater or equal to 0");
+        } else if (parameters.maximumMutationCount() > parameters.geneCount()) {
+            throw new IllegalArgumentException("Maximal mutation count should be less than or equal to gene count");
+        } else if (parameters.geneCount() <= 0) {
+            throw new IllegalArgumentException("Gene count should be greater than 0");
         }
 
-        this.moves = new ArrayList<>(moves);
-        this.map = map;
+        // Get plant growth indicator variant
+        /*PlantGrowthIndicator plantGrowthIndicator = switch (parameters.plantGrowthIndicatorVariant()) {
+            case "Forested equators" -> ...
+        }*/
 
-        this.animals = new ArrayList<>(positions.size());
-        for (Vector2D position : positions) {
-            Animal animal = new Animal(position);
+        // Get animal behaviour indicator variant
+        /*AnimalBehaviourIndicator animalBehaviourIndicator = switch (parameters.animalBehaviourIndicatorVariant()) {
+            case "Full predestination" -> ...
+        };*/
 
-            try {
-                this.map.place(animal);
-                this.animals.add(animal);
-            } catch (PositionAlreadyOccupiedException ignored) {}
-        }
+        // Set map
+        /*this.map = switch (parameters.worldMapVariant()) {
+
+        }*/
+
+        // Set genes indicator variant
+        /*this.genesIndicator = switch (parameters.genesIndicatorVariant()) {
+            case "Full randomization" -> ...
+            case "Slight correction" -> ...
+        }*/
     }
 
     @Override
     public void run() {
-        try {
-            for (int i = 0; i < this.moves.size(); i++) {
-                Thread.sleep(500);
-                this.map.move(this.animals.get(i % this.animals.size()), this.moves.get(i));
-            }
-        } catch (InterruptedException ignored) {}
-    }
 
-    List<Animal> getAnimals() {
-        return Collections.unmodifiableList(this.animals);
     }
 }
--- src/main/java/agh/ics/oop/SimulationParameters.java
+++ src/main/java/agh/ics/oop/SimulationParameters.java
@@ -1,14 +1,9 @@
 package agh.ics.oop;
 
-import agh.ics.oop.model.element.AnimalBehaviourIndicator;
-import agh.ics.oop.model.element.GenesIndicator;
-import agh.ics.oop.model.map.PlantGrowthIndicator;
-import agh.ics.oop.model.map.WorldMap;
-
 public record SimulationParameters(
     int mapWidth,
     int mapHeight,
-    Class<WorldMap> worldMapClass,
+    String worldMapVariant,
     int startPlantCount,
     int plantEnergy,
     int dailyPlantGrowth,
@@ -18,28 +13,8 @@ public record SimulationParameters(
     int reproductionEnergy,
     int minimumMutationCount,
     int maximumMutationCount,
-    Class<PlantGrowthIndicator> plantGrowthVariantClass,
-    Class<GenesIndicator> mutationIndicatorClass,
+    String plantGrowthIndicatorVariant,
+    String genesIndicatorVariant,
     int geneCount,
-    Class<AnimalBehaviourIndicator> animalBehaviourIndicatorClass
-) {
-    public SimulationParameters {
-        if (this.mapWidth() <= 0) {
-            throw new IllegalArgumentException("Map width should be greater than 0");
-        } else if (this.mapHeight() <= 0) {
-            throw new IllegalArgumentException("Map height should be greater than 0");
-        } else if (this.startPlantCount() < 0) {
-            throw new IllegalArgumentException("Start plant count should be greater than or equal to 0");
-        } else if (this.dailyPlantGrowth() < 0) {
-            throw new IllegalArgumentException("Daily plant growth should be greater than or equal to 0");
-        } else if (this.startAnimalCount() <= 0) {
-            throw new IllegalArgumentException("Start animal count should be greater than 0");
-        } else if (this.minimumMutationCount() < 0) {
-            throw new IllegalArgumentException("Minimal mutation count should be greater or equal to 0");
-        } else if (this.maximumMutationCount() > this.geneCount()) {
-            throw new IllegalArgumentException("Maximal mutation count should be less than or equal to gene count");
-        } else if (this.geneCount() <= 0) {
-            throw new IllegalArgumentException("Gene count should be greater than 0");
-        }
-    }
-}
+    String animalBehaviourIndicatorVariant
+) {}
