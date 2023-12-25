80bf995ebfc018f0ef3be515153eb75fc5699fea
RCRalph <rcralph@rcralph.me>
2023-12-25 18:46:52 +0100

    Replaced variant switches with reflection

--- src/main/java/agh/ics/oop/Simulation.java
+++ src/main/java/agh/ics/oop/Simulation.java
@@ -7,24 +7,44 @@ import agh.ics.oop.model.element.EnergyParameters;
 import agh.ics.oop.model.element.behaviour.ABitOfMadnessBehaviourIndicator;
 import agh.ics.oop.model.element.behaviour.BackAndForthBehaviourIndicator;
 import agh.ics.oop.model.element.behaviour.BehaviourIndicator;
-import agh.ics.oop.model.element.behaviour.FullPredestinationBehaviourIndicator;
+import agh.ics.oop.model.element.behaviour.CompletePredestinationBehaviourIndicator;
 import agh.ics.oop.model.element.gene.*;
 import agh.ics.oop.model.map.Boundary;
 import agh.ics.oop.model.map.PlantGrowthIndicator;
 import agh.ics.oop.model.map.UndergroundTunnelsWorldMap;
 import agh.ics.oop.model.map.WorldMap;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
 
 public class Simulation implements Runnable {
-    private final SimulationParameters parameters;
+    public static final Map<String, Class<? extends PlantGrowthIndicator>> PLANT_GROWTH_INDICATORS = Map.of(
 
-    private final WorldMap map;
+    );
+
+    public static final Map<String, Class<? extends WorldMap>> WORLD_MAPS = Map.of(
+        "Underground tunnels", UndergroundTunnelsWorldMap.class
+    );
+
+    public static final Map<String, Class<? extends ChildGenesIndicator>> CHILD_GENES_INDICATORS = Map.of(
+        "Complete randomness", CompleteRandomnessChildGenesIndicator.class,
+        "Slight correction", SlightCorrectionChildGenesGenerator.class,
+        "Replacement", ReplacementChildGenesIndicator.class
+    );
+
+    public static final Map<String, Class<? extends BehaviourIndicator>> BEHAVIOUR_INDICATORS = Map.of(
+        "Complete predestination", CompletePredestinationBehaviourIndicator.class,
+        "A bit of madness", ABitOfMadnessBehaviourIndicator.class,
+        "Back and forth", BackAndForthBehaviourIndicator.class
+    );
+
+    private final SimulationParameters parameters;
 
     private final ChildGenesIndicator childGenesIndicator;
 
     private final BehaviourIndicator behaviourIndicator;
+
+    private final WorldMap worldMap;
 
     private final List<Animal> animals = new ArrayList<>();
 
@@ -32,34 +52,113 @@ public class Simulation implements Runnable {
 
     private int currentDay = 0;
 
+    private PlantGrowthIndicator getPlantGrowthIndicator(
+        SimulationParameters parameters
+    ) throws InvalidSimulationConfigurationException {
+        try {
+            return PLANT_GROWTH_INDICATORS
+                .get(parameters.plantGrowthIndicatorVariant())
+                .getDeclaredConstructor(Integer.class, Integer.class, Integer.class)
+                .newInstance(
+                    parameters.geneCount(),
+                    parameters.minimumMutationCount(),
+                    parameters.maximumMutationCount()
+                );
+        } catch (
+            NoSuchMethodException |
+            InstantiationException |
+            IllegalAccessException |
+            InvocationTargetException exception
+        ) {
+            throw new InvalidSimulationConfigurationException(
+                "Invalid plant growth indicator implementation: " + exception.getMessage()
+            );
+        }
+    }
+
+    private WorldMap getWorldMap(
+        SimulationParameters parameters,
+        PlantGrowthIndicator plantGrowthIndicator
+    ) throws InvalidSimulationConfigurationException {
+        Class<? extends WorldMap> worldMap = WORLD_MAPS.get(parameters.worldMapVariant());
+
+        try {
+            if (worldMap == UndergroundTunnelsWorldMap.class) {
+                return worldMap.getDeclaredConstructor(
+                    Integer.class, Integer.class,
+                    Integer.class, PlantGrowthIndicator.class
+                ).newInstance(
+                    parameters.mapWidth(), parameters.mapHeight(),
+                    parameters.tunnelCount(), plantGrowthIndicator
+                );
+            } else {
+                throw new InvalidSimulationConfigurationException("World map variant should point to a valid class");
+            }
+        } catch (
+            NoSuchMethodException |
+            InstantiationException |
+            IllegalAccessException |
+            InvocationTargetException exception
+        ) {
+            throw new InvalidSimulationConfigurationException(
+                "Invalid world map implementation: " + exception.getMessage()
+            );
+        }
+    }
+
+    private BehaviourIndicator getBehaviourIndicator(
+        SimulationParameters parameters
+    ) throws InvalidSimulationConfigurationException {
+        try {
+            return BEHAVIOUR_INDICATORS
+                .get(parameters.animalBehaviourIndicatorVariant())
+                .getDeclaredConstructor(Integer.class)
+                .newInstance(parameters.geneCount());
+        } catch (
+            NoSuchMethodException |
+            InstantiationException |
+            IllegalAccessException |
+            InvocationTargetException exception
+        ) {
+            throw new InvalidSimulationConfigurationException(
+                "Invalid behaviour indicator implementation: " + exception.getMessage()
+            );
+        }
+    }
+
+    private ChildGenesIndicator getChildGenesIndicator(
+        SimulationParameters parameters
+    ) throws InvalidSimulationConfigurationException {
+        try {
+            return CHILD_GENES_INDICATORS
+                .get(parameters.childGenesIndicatorVariant())
+                .getDeclaredConstructor(Integer.class, Integer.class, Integer.class)
+                .newInstance(
+                    parameters.geneCount(),
+                    parameters.minimumMutationCount(),
+                    parameters.maximumMutationCount()
+                );
+        } catch (
+            NoSuchMethodException |
+            InstantiationException |
+            IllegalAccessException |
+            InvocationTargetException exception
+        ) {
+            throw new InvalidSimulationConfigurationException(
+                "Invalid child genes indicator implementation: " + exception.getMessage()
+            );
+        }
+    }
+
     public Simulation(SimulationParameters parameters) throws InvalidSimulationConfigurationException {
-        if (parameters.mapWidth() <= 0) {
-            throw new InvalidSimulationConfigurationException("Map width should be greater than 0");
-        } else if (parameters.mapHeight() <= 0) {
-            throw new InvalidSimulationConfigurationException("Map height should be greater than 0");
-        } else if (parameters.startPlantCount() < 0) {
-            throw new InvalidSimulationConfigurationException("Start plant count should be greater than or equal to 0");
-        } else if (parameters.dailyPlantGrowth() < 0) {
-            throw new InvalidSimulationConfigurationException("Daily plant growth should be greater than or equal to 0");
-        } else if (parameters.startAnimalCount() <= 0) {
-            throw new InvalidSimulationConfigurationException("Start animal count should be greater than 0");
-        } else if (parameters.animalStartEnergy() < 0) {
-            throw new InvalidSimulationConfigurationException("Animal's start energy should be greater than 0");
-        } else if (parameters.plantEnergy() < 0) {
-            throw new InvalidSimulationConfigurationException("Plant's energy should be greater than 0");
-        } else if (0 < parameters.healthyAnimalEnergy() && parameters.healthyAnimalEnergy() > parameters.reproductionEnergy()) {
-            throw new InvalidSimulationConfigurationException("Reproduction energy should not exceed healthy animal energy level");
-        } else if (parameters.reproductionEnergy() < 0) {
-            throw new InvalidSimulationConfigurationException("Reproduction energy should be greater than 0");
-        } else if (parameters.minimumMutationCount() < 0) {
-            throw new InvalidSimulationConfigurationException("Minimal mutation count should be greater or equal to 0");
-        } else if (parameters.maximumMutationCount() > parameters.geneCount()) {
-            throw new InvalidSimulationConfigurationException("Maximal mutation count should be less than or equal to gene count");
-        } else if (parameters.geneCount() <= 0) {
-            throw new InvalidSimulationConfigurationException("Gene count should be greater than 0");
+        // Java doesn't allow throwing checked exceptions outside lambda expressions,
+        // so this check unfortunately has to be done this way
+        if (parameters.getValidationMessage().isPresent()) {
+            throw new InvalidSimulationConfigurationException(parameters.getValidationMessage().get());
         }
 
         this.parameters = parameters;
+
         this.energyParameters = new EnergyParameters(
             this.parameters.animalStartEnergy(),
             this.parameters.animalMoveEnergy(),
@@ -67,75 +166,18 @@ public class Simulation implements Runnable {
             this.parameters.reproductionEnergy()
         );
 
-        // Set plant growth indicator variant
-        PlantGrowthIndicator plantGrowthIndicator = null; /*switch (parameters.plantGrowthIndicatorVariant()) {
-            case "Forested equators" -> ...
-        }*/
-
-        this.behaviourIndicator = switch (parameters.animalBehaviourIndicatorVariant()) {
-            case "Full predestination" -> new FullPredestinationBehaviourIndicator(parameters.geneCount());
-
-            case "A bit of madness" -> new ABitOfMadnessBehaviourIndicator(parameters.geneCount());
-
-            case "Back and forth" -> new BackAndForthBehaviourIndicator(parameters.geneCount());
-
-            default -> throw new InvalidSimulationConfigurationException(
-                "Animal behaviour indicator should indicate a valid class name"
-            );
-        };
-
-        this.map = switch (parameters.worldMapVariant()) {
-            case "Underground tunnels" -> {
-                if (parameters.tunnelCount() * 2 <= (parameters.mapWidth() + 1) * (parameters.mapHeight() + 1)) {
-                    throw new InvalidSimulationConfigurationException(
-                        "Tunnel entrances count should not exceed total position count of the given map"
-                    );
-                }
-
-                yield new UndergroundTunnelsWorldMap(
-                    parameters.mapWidth(),
-                    parameters.mapHeight(),
-                    parameters.tunnelCount(),
-                    plantGrowthIndicator
-                );
-            }
-            default -> throw new InvalidSimulationConfigurationException(
-                "World map should indicate a valid class name"
-            );
-        };
-
-        this.childGenesIndicator = switch (parameters.childGenesIndicatorVariant()) {
-            case "Full randomization" -> new CompleteRandomnessChildGenesIndicator(
-                parameters.geneCount(),
-                parameters.minimumMutationCount(),
-                parameters.maximumMutationCount()
-            );
-
-            case "Slight correction" -> new SlightCorrectionChildGenesGenerator(
-                parameters.geneCount(),
-                parameters.minimumMutationCount(),
-                parameters.maximumMutationCount()
-            );
-
-            case "Replacement" -> new ReplacementChildGenesIndicator(
-                parameters.geneCount(),
-                parameters.minimumMutationCount(),
-                parameters.maximumMutationCount()
-            );
-
-            default -> throw new InvalidSimulationConfigurationException(
-                "Child genes indicator indicate a valid class name"
-            );
-        };
+        this.worldMap = this.getWorldMap(parameters, this.getPlantGrowthIndicator(parameters));
+        this.behaviourIndicator = this.getBehaviourIndicator(parameters);
+        this.childGenesIndicator = this.getChildGenesIndicator(parameters);
     }
 
     private void placeAnimal(Animal animal) {
         this.animals.add(animal);
-        this.map.placeAnimal(animal);
+        this.worldMap.placeAnimal(animal);
     }
 
     private void generateAnimals() {
-        Boundary boundary = this.map.getCurrentBounds();
+        Boundary boundary = this.worldMap.getCurrentBounds();
 
         for (int i = 0; i < this.parameters.startAnimalCount(); i++) {
             Animal animal = new Animal(
@@ -151,8 +193,8 @@ public class Simulation implements Runnable {
     }
 
     private void reproduceAnimals() {
-        for (Vector2D position : this.map.getAnimalPositions()) {
-            List<Animal> animals = this.map.objectsAt(position)
+        for (Vector2D position : this.worldMap.getAnimalPositions()) {
+            List<Animal> animals = this.worldMap.objectsAt(position)
                 .stream()
                 .filter(item -> item instanceof Animal)
                 .map(item -> (Animal) item)
@@ -182,16 +224,16 @@ public class Simulation implements Runnable {
     @Override
     public void run() {
         this.generateAnimals();
-        this.map.growPlants(this.parameters.startPlantCount());
+        this.worldMap.growPlants(this.parameters.startPlantCount());
 
-        while (this.map.aliveAnimalCount() > 0) {
+        while (this.worldMap.aliveAnimalCount() > 0) {
             this.currentDay++;
 
-            this.map.removeDeadAnimals(this.currentDay);
-            this.map.moveAnimals();
-            this.map.consumePlants();
+            this.worldMap.removeDeadAnimals(this.currentDay);
+            this.worldMap.moveAnimals();
+            this.worldMap.consumePlants();
             this.reproduceAnimals();
-            this.map.growPlants(this.parameters.dailyPlantGrowth());
+            this.worldMap.growPlants(this.parameters.dailyPlantGrowth());
         }
     }
 }
