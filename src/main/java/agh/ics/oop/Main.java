c3232e807cc167660c3d934b8b2ba5d73cf02946
RCRalph <rcralph@rcralph.me>
2023-12-25 18:45:30 +0100

    Renamed SimulationApp to SimulationConfiguration

--- src/main/java/agh/ics/oop/Main.java
+++ src/main/java/agh/ics/oop/Main.java
@@ -5,7 +5,7 @@ import javafx.application.Application;
 public class Main {
     public static void main(String[] args) {
         try {
-            Application.launch(SimulationApp.class, args);
+            Application.launch(SimulationConfiguration.class, args);
         } catch (IllegalArgumentException exception) {
             System.err.println(exception.getMessage());
         }
